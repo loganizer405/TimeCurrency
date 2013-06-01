82525ed2029aa1817a0026e8e82e3e667bcb8a79
Logan Kramer <loganizer405@gmail.com>
2013-06-01 19:56:57 -0400

    Progress!

--- PluginMain.cs
+++ PluginMain.cs
@@ -36,17 +36,14 @@ namespace TimeCurrency
         {
             get { return "Has a currency of time."; }
         }
-
         public override string Name
         {
             get { return "TimeCurrency"; }
         }
-
         public override Version Version
         {
             get { return new Version("1.0"); }
         }
-
 
         public TimeCurrency(Main game)
             : base(game)
@@ -60,17 +57,14 @@ namespace TimeCurrency
                 ServerHooks.Join -= OnJoin;
                 ServerHooks.Leave -= OnLeave;
                 GameHooks.Update -= OnUpdate; 
-
-
             }
         }
         public override void Initialize ()
         {
-
-
-            Commands.ChatCommands.Add(new Command("time.checktime", CheckTime, "checktime"));
-            Commands.ChatCommands.Add(new Command("time.givetime", AddTime, "givetime"));
+            Commands.ChatCommands.Add(new Command("time.default", CheckTime, "checktime"));
+            Commands.ChatCommands.Add(new Command("time.default", AddTime, "givetime"));
             Commands.ChatCommands.Add(new Command("time.subtracttime", SubtractTime, "subtracttime"));
+            Commands.ChatCommands.Add(new Command("time.default", CMDTime, "time"));
 
             ServerHooks.Join += OnJoin;
             ServerHooks.Leave += OnLeave;
@@ -85,15 +79,12 @@ namespace TimeCurrency
             if(!TShock.Groups.GroupExists("dead"))
             {
                 TShock.Groups.AddGroup("dead", "default", "", "255,255,255", false);
-            }         
-            
+            }                    
             foreach (Group group in TShock.Groups)
             {
                 if (group.Name != "superadmin")
-                {
-                   
-                    TShock.Groups.AddPermissions(group.Name, (new List<string>() { "time.checktime", "" }));
-                   
+                {                   
+                    TShock.Groups.AddPermissions(group.Name, (new List<string>() { "time.checktime", "" }));         
                 }
             }
 
@@ -113,11 +104,9 @@ namespace TimeCurrency
                         if (player.TileX == lasttileX[player.Index] && player.TileY == lasttileY[player.Index])//if player is afk
                         {
                             TimePlayed[player.Index] = TimePlayed[player.Index] + 1;
-                        }
-                                                  
-                        lasttileX[player.Index] = player.TileX;
-                        lasttileY[player.Index] = player.TileY;
-                        
+                        }                                              
+                            lasttileX[player.Index] = player.TileX;
+                            lasttileY[player.Index] = player.TileY;                      
                    }
                 }
             }
@@ -203,7 +192,78 @@ namespace TimeCurrency
 
         #region Commands
 
+        void CMDTime(CommandArgs args)
+        {
+            switch(args.Parameters[0])
+            {
+                case "give":
+                    if (args.Player.IsLoggedIn)
+                    {
+                        if (SqlManager.CheckDeadStatus(args.Player.Name))
+                        {
+                            args.Player.SendErrorMessage("You cannot transfer time because you are dead.");
+                        }
+                        if (!SqlManager.CheckDeadStatus(args.Player.Name))
+                        {
+                            if (!args.Player.Group.HasPermission("time.addtime"))
+                            {
+                                if (args.Parameters.Count != 2)
+                                {
+                                    args.Player.SendErrorMessage("Invalid syntax! Proper syntax: /time give <player> <time>");
+                                    args.Player.SendErrorMessage("Syntax for time: 0d0h0m0s");
+                                    return;
+                                }
+                                int time;
 
+                                var player = TShock.Utils.FindPlayer(args.Parameters[0]);
+                                if (player.Count == 0)
+                                {
+                                    args.Player.SendMessage("No players matched!!", Color.OrangeRed);
+                                }
+                                else if (player.Count > 1)
+                                {
+                                    args.Player.SendMessage("More than one player matched!", Color.OrangeRed);
+                                }
+                                else if (player.Count == 1)
+                                {
+                                    if (GetTime(args.Parameters[1], out time) && time > 0)
+                                    {
+                                        //time is the anount of seconds
+
+                                        if (SqlManager.RemoveSeconds(args.Player.Name, time) && SqlManager.AddSeconds(player[0].Name, time))
+                                        {
+                                            args.Player.SendSuccessMessage("Successfully transfered " + args.Parameters[1] + " to " + player[0].Name + " 's account.");
+                                            int time2 = SqlManager.ReadTime(args.Player.Name);
+                                            if (!SqlManager.CheckDeadStatus(args.Player.Name))
+                                            {
+                                                TimeSpan t = TimeSpan.FromSeconds(time2);
+                                                args.Player.SendMessage("Your currenct balance: " + t.Days + " days, " + t.Hours + " hours, " + t.Minutes + " minutes, and " + t.Seconds + " seconds.", Color.LightGreen);
+                                            }
+                                        }
+                                        else
+                                        {
+                                            args.Player.SendErrorMessage("Failed to transfer " + args.Parameters[1] + " to " + player[0].Name + " 's account.");
+                                        }
+                                    }
+                                    else
+                                    {
+                                        args.Player.SendErrorMessage("Invalid time. Syntax: 0d0h0m0s");
+                                    }
+                                }
+                            }
+
+                        }
+                        
+                    }
+                        
+                    
+                        break;
+                default:
+                    args.Player.SendErrorMessage("Invalid argument! Proper arguments: ");
+                    break;
+              
+            }
+        }
         void AddTime(CommandArgs args)
         {
             if (args.Parameters.Count != 2)
@@ -225,16 +285,45 @@ namespace TimeCurrency
             }
             else if (player.Count == 1)
             {
+                if (args.Player.Group.HasPermission("time.add"))
+                {
+                    if (GetTime(args.Parameters[1], out time) && time > 0)
+                    {
+                        //time is the anount of seconds
+                        SqlManager.AddSeconds(player[0].Name, time);
+                        args.Player.SendSuccessMessage("Successfully transfered " + args.Parameters[1] + " to " + player[0].Name + " 's account.");
+                    }
+                    else
+                    {
+                        args.Player.SendErrorMessage("Invalid time. Syntax: 0d0h0m0s");
+                    }
+                }
+                else 
+                {
+                    if (GetTime(args.Parameters[1], out time) && time > 0)
+                    {
+                        //time is the anount of seconds
+                        if (SqlManager.RemoveSeconds(args.Player.Name, time) && SqlManager.AddSeconds(player[0].Name, time))
+                        {
+                            args.Player.SendSuccessMessage("Successfully transfered " + args.Parameters[1] + " to " + player[0].Name + " 's account.");
+                             int time2 = SqlManager.ReadTime(args.Player.Name);
+                             if (!SqlManager.CheckDeadStatus(args.Player.Name))
+                             {
+                                 TimeSpan t = TimeSpan.FromSeconds(time2);
+                                 args.Player.SendMessage("Your currenct balance: " + t.Days + " days, " + t.Hours + " hours, " + t.Minutes + " minutes, and " + t.Seconds + " seconds.", Color.LightGreen);
+                             }
+                        }
+                        else
+                        {
+                            args.Player.SendErrorMessage("Failed to transfer " + args.Parameters[1] + " to " + player[0].Name + " 's account.");
+                        }
+                    }
+                    else
+                    {
+                        args.Player.SendErrorMessage("Invalid time. Syntax: 0d0h0m0s");
+                    }
+                }
 
-                if (GetTime(args.Parameters[1], out time) && time > 0)
-                {
-                    //time is the anount of seconds
-                    SqlManager.RemoveSeconds(player[0].Name, time);
-                }
-                else
-                {
-                    args.Player.SendErrorMessage("Invalid time. Syntax: 0d0h0m0s");
-                }
             }
         }
         void SubtractTime(CommandArgs args)
